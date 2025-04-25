1aaf9ca800131d22d93a798b990f4bf27acbf490
J24l <149522994+J24l@users.noreply.github.com>
2025-04-25 17:21:59 -0400

    Update MenuItemIngredientRepository.java

--- MenuItemIngredientRepository.java
+++ MenuItemIngredientRepository.java
@@ -2,6 +2,8 @@ package com.example.pizzadashboard.repository;
 
 import com.example.pizzadashboard.model.MenuItemIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// The interface extends JpaRepository, which provides basic methods for database operations
 public interface MenuItemIngredientRepository extends JpaRepository<MenuItemIngredient, Long> {
+    // You don't need to write anything here for basic CRUD operations,
+    // List<MenuItemIngredient> findByMenuItemId(Long menuItemId);
 }
