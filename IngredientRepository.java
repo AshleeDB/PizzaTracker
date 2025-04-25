2bc8118134393c8d78406ea795ce72eab4a4d7df
J24l <149522994+J24l@users.noreply.github.com>
2025-04-25 17:14:04 -0400

    Update IngredientRepository.java

--- IngredientRepository.java
+++ IngredientRepository.java
@@ -2,6 +2,8 @@ package com.example.pizzadashboard.repository;
 
 import com.example.pizzadashboard.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// This interface connects the Ingredient entity to the database
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+   // No need to write anything extra unless we want custom queries
+    // JpaRepository provides all the CRUD methods out of the box
 }
