44c99e9578b15ca2d178c5b9e7cd13286744af9d
Ayishah0218 <akhali24@nyit.edu>
2025-04-27 21:11:50 -0400

    Update EmployeeController.java

--- EmployeeController.java
+++ EmployeeController.java
@@ -5,18 +5,19 @@ import com.example.pizzadashboard.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController// handling http requests
 @RequestMapping("/login")
 @CrossOrigin(origins = "*")
 public class EmployeeController {
 
-    @Autowired
+    @Autowired // Tells Spring to automatically inject an instance of EmployeeRepository
     private EmployeeRepository employeeRepo;
 
     @PostMapping
-    public String login(@RequestParam String username, @RequestParam String password) {
+    public String login(@RequestParam String username, @RequestParam String password) {//
         Employee emp = employeeRepo.findByUsername(username);
-        if (emp != null && emp.getPassword().equals(password)) {
+        if (emp != null && emp.getPassword().equals(password)) {//// Check if employee exists and password matches
+            return "Login successful"; // If valid, return success message
             return "Login successful";
         } else {
             return "Invalid credentials";
